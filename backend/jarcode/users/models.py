1fba81ee36969ea19bb8fed27dc46ba97bfc6613
Michal <michal.golawski.mg@gmail.com>
2025-09-16 19:28:20 +0200

    Created custom User model and custom user manager, made migrations

--- backend/jarcode/users/models.py
+++ backend/jarcode/users/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from .managers import EmailUserManager
+from uuid import uuid4
 
-# Create your models here.
+
+class User(AbstractUser):
+    uuid = models.UUIDField(default=uuid4, blank=False,
+                            null=False, editable=False)
+    is_content_creator = models.BooleanField(blank=False, null=False)
+    email = models.EmailField(("email address"), unique=True)
+    first_name = models.CharField(blank=False, null=False)
+    last_name = models.CharField(blank=False, null=False)
+    username = None
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = EmailUserManager()
+
+    def __str__(self):
+        return self.email
