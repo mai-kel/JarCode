7987d52f354069a3422c152f902b94769040e9b8
Michal <michal.golawski.mg@gmail.com>
2025-10-25 18:04:40 +0200

    Registered user model in admin.py

--- backend/jarcode/users/admin.py
+++ backend/jarcode/users/admin.py
@@ -1,3 +1,63 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = (
+        'email',
+        'first_name',
+        'last_name',
+        'is_content_creator',
+        'is_staff',
+        'is_active',
+    )
+
+    list_filter = (
+        'is_active',
+        'is_staff',
+        'is_superuser',
+        'is_content_creator',
+    )
+
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
+
+    fieldsets = (
+        (None, {
+            'fields': ('email', 'password')
+        }),
+        ('Informacje osobiste', {
+            'fields': ('first_name', 'last_name')
+        }),
+        ('Uprawnienia', {
+            'fields': (
+                'is_active',
+                'is_staff',
+                'is_superuser',
+                'is_content_creator',
+                'groups',
+                'user_permissions'
+            )
+        }),
+        ('Ważne daty', {
+            'fields': ('last_login', 'date_joined', 'uuid')
+        }),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password',
+                'password2',
+                'first_name',
+                'last_name',
+                'is_content_creator'
+            ),
+        }),
+    )
+
+    readonly_fields = ('last_login', 'date_joined', 'uuid')
+    filter_horizontal = ('groups', 'user_permissions',)
